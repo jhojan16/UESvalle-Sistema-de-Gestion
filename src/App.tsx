ca8b7f73916a5c03cbaba1000180c058ed39fb84
Jhojan <147114192+jhojan16@users.noreply.github.com>
2025-11-06 16:05:29 -0500

    FIX: rutas de vercel

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
       <BrowserRouter>
         <AuthProvider>
           <Routes>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
             <Route
               path="/dashboard"
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/login",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
