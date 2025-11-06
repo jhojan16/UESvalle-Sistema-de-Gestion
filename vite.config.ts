b1b20b14071885122382e314918a6c70b1806a90
Jhojan <147114192+jhojan16@users.noreply.github.com>
2025-11-06 16:00:55 -0500

    FIX: rutas de vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/login",
   server: {
     host: "::",
     port: 8080,
